dea2291c307844b4fddea7facec624ab1abb0f41
iFelix18 <19800006+iFelix18@users.noreply.github.com>
2019-02-21 18:18:31 +0100

    2.1.0

--- userscripts/ratings-on-trakt.user.js
+++ userscripts/ratings-on-trakt.user.js
@@ -7,7 +7,7 @@
 // @description:it  Aggiunge valutazioni da IMDb, Rotten Tomatoes e Metacritic a Trakt
 // @copyright       2019, Felix (https://github.com/iFelix18)
 // @license         MIT
-// @version         2.0.4
+// @version         2.1.0
 // @homepageURL     https://git.io/Trakt-Userscripts
 // @supportURL      https://github.com/iFelix18/Trakt-Userscripts/issues
 // @updateURL       https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/meta/ratings-on-trakt.meta.js
@@ -15,6 +15,7 @@
 // @require         https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js#sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=
 // @require         https://cdn.jsdelivr.net/gh/soufianesakhi/node-creation-observer-js@edabdee1caaee6af701333a527a0afd95240aa3b/release/node-creation-observer-latest.min.js
 // @require         https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@a4a49b47ecfb1d8fcd27049cc0e8114d05522a0f/gm_config.min.js
+// @require         https://cdn.jsdelivr.net/npm/mathjs@5.4.2/dist/math.min.js#sha256-W2xP+GeD3rATAAJ/rtjz0uNLqO9Ve9yk9744ImX8GWY=
 // @require         https://cdn.jsdelivr.net/npm/handlebars@4.0.12/dist/handlebars.min.js#sha256-qlku5J3WO/ehJpgXYoJWC2px3+bZquKChi4oIWrAKoI=
 // @include         https://trakt.tv/*
 // @connect         omdbapi.com
@@ -31,53 +32,112 @@
 (function () {
   'use strict'
 
-  /* global Handlebars */
+  /* global math, Handlebars */
 
-  // observe nodes
-  NodeCreationObserver.onCreation('.shows #summary-wrapper .summary .container', function () {
-    ratingsOnTrakt()
-  })
-  NodeCreationObserver.onCreation('.movies #summary-wrapper .summary .container', function () {
-    ratingsOnTrakt()
-  })
-
-  function ratingsOnTrakt () {
-    // run functions
+  // observe node
+  NodeCreationObserver.init('observed-ratings')
+  NodeCreationObserver.onCreation('.shows #summary-wrapper .summary .container h1', function () {
     addCSS()
-    addSettings()
-    addRatings()
+    getRatings()
+  })
+  NodeCreationObserver.onCreation('.movies #summary-wrapper .summary .container h1', function () {
+    addCSS()
+    getRatings()
+  })
+
+  // get Metacritic color
+  function getMetascoreColor (rating) {
+    let color = 0
+    if (rating < 40) {
+      color = '#FF0000'
+    } else if (rating >= 40 && rating <= 60) {
+      color = '#FFCC33'
+    } else if (rating > 60) {
+      color = '#66CC33'
+    }
+    return color
   }
 
-  function addSettings () {
-    // settings configuration
-    GM_config.init({
-      'id': 'MyConfig',
-      'title': 'Ratings on Trakt - Settings',
-      'fields': {
-        'apikey': {
-          'label': 'OMDb API Key',
-          'section': ['You can request a free OMDb API Key at:', 'https://www.omdbapi.com/apikey.aspx'],
-          'type': 'text',
-          'default': ''
-        }
-      },
-      'events': {
-        'save': function () {
-          alert('Settings saved')
-          location.reload()
-        }
-      }
-    })
-
-    // add settings on add-on menu
-    GM_registerMenuCommand('Ratings on Trakt - Configure', function () {
-      GM_config.open()
-    })
-
-    // controll apikey
-    if (GM_config.get('apikey') === '') {
-      GM_config.open()
+  // get Fresh or Rotten
+  function getTomatometer (rating) {
+    let tomatometer = 0
+    if (parseFloat(rating) < 60) {
+      tomatometer = 'Rotten'
+    } else {
+      tomatometer = 'Fresh'
     }
+    return tomatometer
+  }
+
+  // get RottenTomatoes Logo
+  function getRottenTomatoesLogo (rating) {
+    let tomatoLogo = 0
+    if (parseFloat(rating) < 60) {
+      tomatoLogo = 'https://www.rottentomatoes.com/assets/pizza-pie/images/icons/global/new-rotten-lg.ecdfcf9596f.png'
+    } else {
+      tomatoLogo = 'https://www.rottentomatoes.com/assets/pizza-pie/images/icons/global/new-fresh-lg.12e316e31d2.png'
+    }
+    return tomatoLogo
+  }
+
+  // add ratings
+  function addMetacriticRating (rating) {
+    // add HTML structure
+    $(`
+      <script id="Metacritic-rating-template" type="text/x-handlebars-template">
+        <div class="rated-text">
+          <div class="icon">
+            <img class="Metacritic logo" src="https://upload.wikimedia.org/wikipedia/commons/2/20/Metacritic.svg">
+          </div>
+          <div class="number">
+            <div class="rating">{{rating}}</div>
+            <div class="votes" style="margin-top: 4px; height: 6px; width: 100%; background-color: rgba(0, 0, 0, 0.5);">
+              <div class="bar" style="height: 6px; width: {{rating}}%; background-color: {{color}};"></div>
+            </div>
+          </div>
+        </div>
+      </script>
+      <li class="Metacritic-rating"></li>
+    `).appendTo($('#summary-ratings-wrapper .ratings'))
+
+    // compile HTML structure
+    let template = Handlebars.compile($('#Metacritic-rating-template').html())
+    let context = {
+      'rating': rating,
+      'color': getMetascoreColor(rating)
+    }
+    let compile = template(context)
+    $('.Metacritic-rating').html(compile)
+    console.log('Ratings on Trakt: Metacritic rating added')
+  }
+
+  function addRottenTomatoesRating (rating) {
+    // add HTML structure
+    $(`
+      <script id="RottenTomatoes-rating-template" type="text/x-handlebars-template">
+        <div class="rated-text">
+          <div class="icon">
+            <img class="RottenTomatoes logo" src="{{logo}}">
+          </div>
+          <div class="number">
+            <div class="rating">{{rating}}</div>
+            <div class="votes">{{tomatometer}}</div>
+          </div>
+        </div>
+      </script>
+      <li class="RottenTomatoes-rating"></li>
+    `).appendTo($('#summary-ratings-wrapper .ratings'))
+
+    // compile HTML structure
+    let template = Handlebars.compile($('#RottenTomatoes-rating-template').html())
+    let context = {
+      'logo': getRottenTomatoesLogo(rating),
+      'rating': rating,
+      'tomatometer': getTomatometer(rating)
+    }
+    let compile = template(context)
+    $('.RottenTomatoes-rating').html(compile)
+    console.log('Ratings on Trakt: Rotten Tomatoes rating added')
   }
 
   function addIMDbRating (rating, votes) {
@@ -105,148 +165,110 @@
     }
     let compile = template(context)
     $('.IMDb-rating').html(compile)
-
     console.log('Ratings on Trakt: IMDb rating added')
   }
 
-  function addRottenTomatoesRating (logo, rating, tomato) {
-    // add HTML structure
-    $(`
-      <script id="RottenTomatoes-rating-template" type="text/x-handlebars-template">
-        <div class="rated-text">
-          <div class="icon">
-            <img class="RottenTomatoes logo" src="{{logo}}">
-          </div>
-          <div class="number">
-            <div class="rating">{{rating}}</div>
-            <div class="votes">{{tomato}}</div>
-          </div>
-        </div>
-      </script>
-      <li class="RottenTomatoes-rating"></li>
-    `).appendTo($('#summary-ratings-wrapper .ratings'))
+  function addRatings (json) {
+    if (json && json.Response === 'True') {
+      // IMDb
+      if (json.imdbRating && json.imdbRating !== 'N/A' && json.imdbVotes && json.imdbVotes !== 'N/A') {
+        let rating = json.imdbRating
+        let votes = math.round((json.imdbVotes.replace(/,/g, '')) / 1000, 1)
+        console.log(`Ratings on Trakt: IMDb rating is "${rating}"`)
+        console.log(`Ratings on Trakt: IMDb votes is "${votes}k"`)
+        addIMDbRating(rating, votes)
+      }
 
-    // compile HTML structure
-    let template = Handlebars.compile($('#RottenTomatoes-rating-template').html())
-    let context = {
-      'logo': logo,
-      'rating': rating,
-      'tomato': tomato
+      // Rotten Tomatoes
+      if (json.Ratings[1] && json.Ratings[1] !== 'undefined' && json.Ratings[1].Source === 'Rotten Tomatoes' && json.Ratings[1].Value) {
+        let rating = json.Ratings[1].Value
+        console.log(`Ratings on Trakt: Tomatometer is "${rating}"`)
+        addRottenTomatoesRating(rating)
+      }
+
+      // Metacritic
+      if (json.Metascore && json.Metascore !== 'N/A' && json.Metascore) {
+        let rating = json.Metascore
+        console.log(`Ratings on Trakt: Metascore is "${rating}"`)
+        addMetacriticRating(rating)
+      }
     }
-    let compile = template(context)
-    $('.RottenTomatoes-rating').html(compile)
-
-    console.log('Ratings on Trakt: Rotten Tomatoes rating added')
   }
 
-  function addMetacriticRating (rating, color) {
-    // add HTML structure
-    $(`
-      <script id="Metacritic-rating-template" type="text/x-handlebars-template">
-        <div class="rated-text">
-          <div class="icon">
-            <img class="Metacritic logo" src="https://upload.wikimedia.org/wikipedia/commons/2/20/Metacritic.svg">
-          </div>
-          <div class="number">
-            <div class="rating">{{rating}}</div>
-            <div class="votes" style="margin-top: 4px; height: 6px; width: 100%; background-color: rgba(0, 0, 0, 0.5);">
-              <div class="bar" style="height: 6px; width: {{rating}}%; background-color: {{color}};"></div>
-            </div>
-          </div>
-        </div>
-      </script>
-      <li class="Metacritic-rating"></li>
-    `).appendTo($('#summary-ratings-wrapper .ratings'))
+  // get ratings
+  function getRatings () {
+    console.log(`Ratings on Trakt: OMDb API Key is "${apikey()}"`)
+    console.log(`Ratings on Trakt: IMDb ID is "${id()}"`)
 
-    // compile HTML structure
-    let template = Handlebars.compile($('#Metacritic-rating-template').html())
-    let context = {
-      'rating': rating,
-      'color': color
-    }
-    let compile = template(context)
-    $('.Metacritic-rating').html(compile)
-
-    console.log('Ratings on Trakt: Metacritic rating added')
-  }
-
-  function addRatings () {
-    // get apikey
-    let apikey = GM_config.get('apikey')
-    console.log(`Ratings on Trakt: OMDb API Key is ${apikey}`)
-
-    // get IMDb ID
-    let IMDbID = $("a[href*='imdb']").attr('href').match(/tt\d+/)[0]
-    console.log(`Ratings on Trakt: IMDb ID is ${IMDbID}`)
-
-    // get ratings
     GM_xmlhttpRequest({
       method: 'GET',
-      url: `http://www.omdbapi.com/?apikey=${apikey}&i=${IMDbID}`,
+      url: `http://www.omdbapi.com/?apikey=${apikey()}&i=${id()}`,
       onload: function (response) {
         let json = JSON.parse(response.responseText)
-        // add IMDb ratings
-        if (json && json.Response !== 'False' && json.imdbRating && json.imdbRating !== 'N/A' && json.imdbVotes && json.imdbVotes !== 'N/A') {
-          console.log(`Ratings on Trakt: IMDb rating is ${json.imdbRating}`)
-          console.log(`Ratings on Trakt: IMDb votes is ${((json.imdbVotes.replace(/,/g, '')) / 1000).toFixed(1)}k`)
-          addIMDbRating(json.imdbRating, ((json.imdbVotes.replace(/,/g, '')) / 1000).toFixed(1))
-        } else {
-          console.log('Ratings on Trakt: IMDb rating not available')
-        }
-
-        // add Rotten Tomatoes ratings
-        if (json && json.Response !== 'False' && json.Ratings[1] && json.Ratings[1] !== 'undefined' && json.Ratings[1].Source === 'Rotten Tomatoes' && json.Ratings[1].Value) {
-          console.log(`Ratings on Trakt: Tomatometer is ${json.Ratings[1].Value}`)
-          let tomatometer = 0
-          let tomatoLogo = 0
-          if (parseFloat(json.Ratings[1].Value) < 60) {
-            tomatometer = 'Rotten'
-            tomatoLogo = 'https://www.rottentomatoes.com/assets/pizza-pie/images/icons/global/new-rotten-lg.ecdfcf9596f.png'
-          } else {
-            tomatometer = 'Fresh'
-            tomatoLogo = 'https://www.rottentomatoes.com/assets/pizza-pie/images/icons/global/new-fresh-lg.12e316e31d2.png'
+        if (json && json.Response === 'False' && json.Error) { // error
+          let message = `Ratings on Trakt: error "${json.Error}"`
+          if (json.Error === 'Invalid API key!') { // if invalid API Key
+            alert(message)
+            GM_config.open()
+          } else { // other errors
+            console.log(message)
           }
-          console.log(`Ratings on Trakt: Rotten or Fresh? ${tomatometer}`)
-          addRottenTomatoesRating(tomatoLogo, json.Ratings[1].Value, tomatometer)
         } else {
-          console.log('Ratings on Trakt: Tomatometer not available')
-        }
-
-        // add Metacritic ratings
-        if (json && json.Response !== 'False' && json.Metascore && json.Metascore !== 'N/A' && json.Metascore < 40) {
-          console.log(`Ratings on Trakt: Metascore is ${json.Metascore}`)
-          addMetacriticRating(json.Metascore, '#FF0000')
-        } else if (json && json.Response !== 'False' && json.Metascore && json.Metascore !== 'N/A' && json.Metascore >= 40 && json.Metascore <= 60) {
-          console.log(`Ratings on Trakt: Metascore is ${json.Metascore}`)
-          addMetacriticRating(json.Metascore, '#FFCC33')
-        } else if (json && json.Response !== 'False' && json.Metascore && json.Metascore !== 'N/A' && json.Metascore > 60) {
-          console.log(`Ratings on Trakt: Metascore is ${json.Metascore}`)
-          addMetacriticRating(json.Metascore, '#66CC33')
-        } else {
-          console.log('Ratings on Trakt: Metascore not available')
-        }
-
-        // error
-        if (json && json.Response !== 'True' && json.Error) {
-          console.log(`Ratings on Trakt: Error ${json.Error}`)
+          addRatings(json)
         }
       }
     })
   }
 
+  // get IMDb ID
+  function id () {
+    return $("a[href*='imdb']").attr('href').match(/tt\d+/)
+  }
+
+  // get API Key
+  function apikey () {
+    return GM_config.get('apikey')
+  }
+
+  // add CSS
   function addCSS () {
-    // add CSS
     $('head').append(`
       <style type='text/css'>
         #summary-ratings-wrapper ul li {
-          margin: 0 19px 0 0;
+          margin-right: 19px;
+        }
+        #summary-ratings-wrapper ul li:last-child {
+          margin-right: 19px;
         }
         #summary-ratings-wrapper ul.stats {
-          margin-left: 19px;
+          margin-left: 0px;
         }
       </style>
     `)
-
     console.log('Ratings on Trakt: CSS added')
   }
+
+  // settings
+  GM_registerMenuCommand('Ratings on Trakt - Configure', function () {
+    GM_config.open()
+  })
+
+  GM_config.init({
+    'id': 'MyConfig',
+    'title': 'Ratings on Trakt - Settings',
+    'fields': {
+      'apikey': {
+        'label': 'OMDb API Key',
+        'section': ['You can request a free OMDb API Key at:', 'https://www.omdbapi.com/apikey.aspx'],
+        'type': 'text',
+        'default': ''
+      }
+    },
+    'events': {
+      'save': function () {
+        alert('Ratings on Trakt: Settings saved')
+        location.reload()
+      }
+    }
+  })
 })()
