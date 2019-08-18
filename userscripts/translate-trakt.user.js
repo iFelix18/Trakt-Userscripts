66b6c2030229dd0529f0ac1e148f7193679588d6
Felix <19800006+iFelix18@users.noreply.github.com>
2019-08-18 19:43:36 +0200

    2.0.2

--- userscripts/translate-trakt.user.js
+++ userscripts/translate-trakt.user.js
@@ -7,14 +7,14 @@
 // @description:it  Traduce titoli, trame, tagline e poster di film, serie TV ed episodi nella lingua scelta
 // @copyright       2019, Felix (https://github.com/iFelix18)
 // @license         MIT
-// @version         2.0.1
+// @version         2.0.2
 // @homepageURL     https://git.io/Trakt-Userscripts
 // @homepageURL     https://greasyfork.org/scripts/377969-translate-trakt
 // @homepageURL     https://openuserjs.org/scripts/iFelix18/Translate_Trakt
 // @supportURL      https://github.com/iFelix18/Trakt-Userscripts/issues
 // @updateURL       https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/meta/translate-trakt.meta.js
 // @downloadURL     https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/translate-trakt.user.js
-// @require         https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js#sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=
+// @require         https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js#sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=
 // @require         https://cdn.jsdelivr.net/gh/soufianesakhi/node-creation-observer-js@edabdee1caaee6af701333a527a0afd95240aa3b/release/node-creation-observer-latest.min.js
 // @require         https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@a4a49b47ecfb1d8fcd27049cc0e8114d05522a0f/gm_config.min.js
 // @require         https://cdn.jsdelivr.net/gh/cvzi/RequestQueue@e4297b3c2e11761d69858bad4746832ea412b571/RequestQueue.min.js
@@ -32,38 +32,57 @@
 // Recommended in combination with Darkt, my darker theme for Trakt.
 // More info on: https://git.io/Darkt
 
+/* global $, NodeCreationObserver, GM_config, RequestQueue */
+
 (function () {
   'use strict'
 
-  /* global $, NodeCreationObserver, GM_config, RequestQueue */
+  console.log(`${GM_info.script.name} v${GM_info.script.version} by Felix is running!`)
 
-  // settings
-  GM_registerMenuCommand(`${GM_info.script.name} - Configure`, function () {
-    GM_config.open()
-  })
+  // configuration
   GM_config.init({
-    'id': 'MyConfigs',
-    'title': GM_info.script.name,
-    'fields': {
-      'apikey': {
-        'label': 'TMDb API Key',
-        'section': ['Enter your TMDb API Key', 'Get one at: https://developers.themoviedb.org/3/'],
-        'type': 'text',
-        'default': ''
+    id: 'trakt-config',
+    title: `${GM_info.script.name} Settings`,
+    fields: {
+      apikey: {
+        label: 'TMDb API Key',
+        section: ['Enter your TMDb API Key', 'Get one at: https://developers.themoviedb.org/3/'],
+        type: 'text',
+        default: ''
       },
-      'language': {
-        'label': 'Language',
-        'section': ['Enter the code of your language, for example: en-US, it-IT, fr-FR.', 'More info at: https://developers.themoviedb.org/3/getting-started/languages'],
-        'type': 'text',
-        'default': 'en-US'
+      language: {
+        label: 'Language',
+        section: ['Enter the code of your language, for example: en-US, it-IT, fr-FR.', 'More info at: https://developers.themoviedb.org/3/getting-started/languages'],
+        type: 'text',
+        default: 'en-US'
+      },
+      logging: {
+        label: 'Logging',
+        labelPos: 'above',
+        type: 'checkbox',
+        default: false
       }
     },
-    'events': {
-      'save': function () {
+    css: '#trakt-config {background-color: #343434; color: #fff;} #trakt-config * {font-family: varela round,helvetica neue,Helvetica,Arial,sans-serif;} #trakt-config .section_header {background-color: #282828; border: 1px solid #282828; border-bottom: none; color: #fff; font-size: 10pt;} #trakt-config .section_desc {background-color: #282828; border: 1px solid #282828; border-top: none; color: #fff; font-size: 10pt;} #trakt-config .reset {color: #fff;}',
+    events: {
+      save: () => {
+        alert(`${GM_info.script.name} : Settings saved`)
         location.reload()
       }
     }
   })
+
+  // menu command to open configuration
+  GM_registerMenuCommand(`${GM_info.script.name} - Configure`, () => {
+    GM_config.open()
+  })
+
+  // logs
+  const log = message => {
+    if (GM_config.get('logging') === true) {
+      console.log(`${GM_info.script.name}: ${message}`)
+    }
+  }
 
   // NodeCreationObserver
   NodeCreationObserver.init('observed-translate')
@@ -72,7 +91,7 @@
       method: 'GET',
       url: `https://api.themoviedb.org/3/movie/${TMDbID()}?api_key=${apikey()}&language=${language()}`,
       onload: function (response) {
-        let data = JSON.parse(response.responseText)
+        const data = JSON.parse(response.responseText)
         if (data && data.status_message) {
           error(data.status_message)
         } else {
@@ -97,7 +116,7 @@
       method: 'GET',
       url: `https://api.themoviedb.org/3/tv/${TMDbID()}?api_key=${apikey()}&language=${language()}`,
       onload: function (response) {
-        let data = JSON.parse(response.responseText)
+        const data = JSON.parse(response.responseText)
         if (data && data.status_message) {
           error(data.status_message)
         } else {
@@ -119,11 +138,11 @@
       method: 'GET',
       url: `https://api.themoviedb.org/3/tv/${TMDbID()}?api_key=${apikey()}&language=${language()}`,
       onload: function (response) {
-        let data = JSON.parse(response.responseText)
+        const data = JSON.parse(response.responseText)
         if (data && data.status_message) {
           error(data.status_message)
         } else {
-          let sn = season()
+          const sn = season()
           if (data && data.seasons[sn].poster_path && data.seasons[sn].poster_path !== null) {
             translatePoster(`https://image.tmdb.org/t/p/w185${data.seasons[sn].poster_path}`)
           }
@@ -141,19 +160,19 @@
     })
   })
   NodeCreationObserver.onCreation('.episode .external a[href*="themoviedb"]', function () {
-    let rq = new RequestQueue(1)
-    let id = TMDbID()
-    let ak = apikey()
-    let ln = language()
-    let sn = season()
-    let en = episode()
+    const rq = new RequestQueue(1)
+    const id = TMDbID()
+    const ak = apikey()
+    const ln = language()
+    const sn = season()
+    const en = episode()
     rq.add({
       method: 'GET',
       url: `https://api.themoviedb.org/3/tv/${id}?api_key=${ak}&language=${ln}`,
       onload: function (response) {
-        let data = JSON.parse(response.responseText)
+        const data = JSON.parse(response.responseText)
         if (data && data.status_message) {
-          console.log(`Translate Trakt: error is "${data.status_message}"`)
+          log(`error is "${data.status_message}"`)
         } else {
           if (data && data.seasons[sn].poster_path && data.seasons[sn].poster_path !== null) {
             translatePoster(`https://image.tmdb.org/t/p/w185${data.seasons[sn].poster_path}`)
@@ -168,7 +187,7 @@
       method: 'GET',
       url: `https://api.themoviedb.org/3/tv/${id}/season/${sn}/episode/${en}?api_key=${ak}&language=${ln}`,
       onload: function (response) {
-        let data = JSON.parse(response.responseText)
+        const data = JSON.parse(response.responseText)
         if (data && data.status_message) {
           error(data.status_message)
         } else {
@@ -185,60 +204,60 @@
 
   // translate overview
   function translateOverview (overview) {
-    console.log(`Translate Trakt: overview is "${short(overview)}"`)
+    log(`overview is "${short(overview)}"`)
     $('#info-wrapper .info #overview p').text(overview)
-    console.log('Translate Trakt: overview is translated')
+    log('overview is translated')
   }
 
   // translate tagline
   function translateTagline (tagline) {
-    console.log(`Translate Trakt: tagline is "${short(tagline)}"`)
+    log(`tagline is "${short(tagline)}"`)
     $('#info-wrapper .info #tagline').text(tagline)
-    console.log('Translate Trakt: tagline is translated')
+    log('tagline is translated')
   }
 
   // translate title
   function translateTitle (title) {
-    console.log(`Translate Trakt: title is "${title}"`)
-    let container = $('#summary-wrapper .summary .container h1')
-    let year = container.find('.year')
-    let certification = container.find('.certification')
+    log(`title is "${title}"`)
+    const container = $('#summary-wrapper .summary .container h1')
+    const year = container.find('.year')
+    const certification = container.find('.certification')
     container.text(title).append(' ').append(year).append(certification)
-    console.log('Translate Trakt: title is translated')
+    log('title is translated')
   }
   function translateSeriesTitle (seriesTitle) {
-    console.log(`Translate Trakt: series title is "${seriesTitle}"`)
+    log(`series title is "${seriesTitle}"`)
     $('#summary-wrapper .summary .container h2 a').text(seriesTitle)
-    console.log('Translate Trakt: series title is translated')
+    log('series title is translated')
   }
   function translateSeasonTitle (title, seasonTitle) {
-    console.log(`Translate Trakt: season title is "${title}: ${seasonTitle}"`)
+    log(`season title is "${title}: ${seasonTitle}"`)
     $('#summary-wrapper .summary .container h2 a:first-child').text(title).append(': ')
     $('#summary-wrapper .summary .container h2 a:last-child').text(seasonTitle)
-    console.log('Translate Trakt: season title is translated')
+    log('season title is translated')
   }
   function translateEpisodeTitle (episodeTitle) {
-    console.log(`Translate Trakt: episode title is "${episodeTitle}"`)
+    log(`episode title is "${episodeTitle}"`)
     $('#summary-wrapper .summary .container h1 .main-title').text(episodeTitle)
-    console.log('Translate Trakt: episode title is translated')
+    log('episode title is translated')
   }
 
   // translate poster
   function translatePoster (poster) {
-    console.log(`Translate Trakt: poster url is "${poster}"`)
+    log(`poster url is "${poster}"`)
     $('#info-wrapper .sidebar .poster .real').removeAttr('data-original').removeAttr('src').attr('src', poster)
     $('#summary-wrapper .mobile-poster .poster .real').removeAttr('data-original').removeAttr('src').attr('src', poster)
-    console.log('Translate Trakt: poster is translated')
+    log('poster is translated')
   }
 
   // error
   function error (error) {
-    let log = `Translate Trakt: error is "${error}"`
+    const log = `error is "${error}"`
     if (error === 'Invalid API key: You must be granted a valid key.') {
-      alert(log)
+      alert(`${GM_info.script.name}: ${log}`)
       GM_config.open()
     } else {
-      console.log(log)
+      log(log)
     }
   }
 
@@ -249,53 +268,53 @@
 
   // get language
   function language () {
-    let language = GM_config.get('language')
+    const language = GM_config.get('language')
     if (language === '') {
-      alert('Translate Trakt: error "Language not set"')
+      alert(`${GM_info.script.name}: error "Language not set"`)
       GM_config.open()
     } else {
-      console.log(`Translate Trakt: language is "${language}"`)
+      log(`language is "${language}"`)
       return language
     }
   }
 
   // get API Key
   function apikey () {
-    let apikey = GM_config.get('apikey')
+    const apikey = GM_config.get('apikey')
     if (apikey === '') {
       GM_config.open()
     } else {
-      console.log(`Translate Trakt: TMDb API Key is "${apikey}"`)
+      log(`TMDb API Key is "${apikey}"`)
       return apikey
     }
   }
 
   // get episode
   function episode () {
-    let link = $('.external a[href*="themoviedb"]')
+    const link = $('.external a[href*="themoviedb"]')
     if (link.length) {
-      let episode = link.attr('href').match(/(episode)\/(\d+)/)[2]
-      console.log(`Translate Trakt: episode is "${episode}"`)
+      const episode = link.attr('href').match(/(episode)\/(\d+)/)[2]
+      log(`episode is "${episode}"`)
       return episode
     }
   }
 
   // get season
   function season () {
-    let link = $('.external a[href*="themoviedb"]')
+    const link = $('.external a[href*="themoviedb"]')
     if (link.length) {
-      let season = link.attr('href').match(/(season)\/(\d+)/)[2]
-      console.log(`Translate Trakt: season is "${season}"`)
+      const season = link.attr('href').match(/(season)\/(\d+)/)[2]
+      log(`season is "${season}"`)
       return season
     }
   }
 
   // get TMDb ID
   function TMDbID () {
-    let link = $('.external a[href*="themoviedb"]')
+    const link = $('.external a[href*="themoviedb"]')
     if (link.length) {
-      let TMDbID = link.attr('href').match(/(movie|tv)\/(\d+)/)[2]
-      console.log(`Translate Trakt: TMDb ID is "${TMDbID}"`)
+      const TMDbID = link.attr('href').match(/(movie|tv)\/(\d+)/)[2]
+      log(`TMDb ID is "${TMDbID}"`)
       return TMDbID
     }
   }
