c9da5b23d0ff36fcbdfe9b58dc13bf839d104000
Felix <19800006+iFelix18@users.noreply.github.com>
2019-11-22 22:53:42 +0100

    1.0.5

--- userscripts/advanced-filtering-on-trakt.user.js
+++ userscripts/advanced-filtering-on-trakt.user.js
@@ -7,20 +7,26 @@
 // @description:it  Mostra i Filtri Avanzati su Trakt
 // @copyright       2019, Felix (https://github.com/iFelix18)
 // @license         MIT
-// @version         1.0.4
+// @version         1.0.5
 // @homepageURL     https://git.io/Trakt-Userscripts
 // @homepageURL     https://greasyfork.org/scripts/383595-advanced-filtering-on-trakt
 // @homepageURL     https://openuserjs.org/scripts/iFelix18/Advanced_Filtering_on_Trakt
 // @supportURL      https://github.com/iFelix18/Trakt-Userscripts/issues
 // @updateURL       https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/meta/advanced-filtering-on-trakt.meta.js
 // @downloadURL     https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/advanced-filtering-on-trakt.user.js
+// @require         https://cdn.jsdelivr.net/gh/greasemonkey/gm4-polyfill@master/gm4-polyfill.min.js
+// @require         https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@master/gm_config.min.js
+// @require         https://cdn.jsdelivr.net/gh/soufianesakhi/node-creation-observer-js@master/release/node-creation-observer-1.2.min.js
+// @require         https://cdn.jsdelivr.net/gh/iFelix18/Userscripts@master/lib/utils/utils.min.js
 // @require         https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js#sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=
-// @require         https://cdn.jsdelivr.net/gh/soufianesakhi/node-creation-observer-js@edabdee1caaee6af701333a527a0afd95240aa3b/release/node-creation-observer-latest.min.js
-// @require         https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@a4a49b47ecfb1d8fcd27049cc0e8114d05522a0f/gm_config.min.js
 // @match           *://trakt.tv/*
-// @grant           GM_info
-// @grant           GM_setValue
+// @grant           GM.getValue
 // @grant           GM_getValue
+// @grant           GM.setValue
+// @grant           GM_setValue
+// @grant           GM.info
+// @grant           GM_info
+// @grant           GM.registerMenuCommand
 // @grant           GM_registerMenuCommand
 // @run-at          document-start
 // @inject-into     page
@@ -29,20 +35,19 @@
 // Recommended in combination with Darkt, my darker theme for Trakt.
 // More info on: https://git.io/Darkt
 
-/* global  $, NodeCreationObserver, GM_config */
+/* global GM_config, NodeCreationObserver, Utils, $ */
 
 (() => {
   'use strict'
 
-  console.log(`${GM_info.script.name} v${GM_info.script.version} by Felix is running!`)
-
-  // configuration
+  //* GM_config
   GM_config.init({
     id: 'trakt-config',
-    title: `${GM_info.script.name} Settings`,
+    title: `${GM.info.script.name} v${GM.info.script.version} Settings`,
     fields: {
       logging: {
         label: 'Logging',
+        section: ['Develop'],
         labelPos: 'above',
         type: 'checkbox',
         default: false
@@ -51,25 +56,25 @@
     css: '#trakt-config{background-color:#343434;color:#fff}#trakt-config *{font-family:varela round,helvetica neue,Helvetica,Arial,sans-serif}#trakt-config .section_header{background-color:#282828;border:1px solid #282828;border-bottom:none;color:#fff;font-size:10pt}#trakt-config .section_desc{background-color:#282828;border:1px solid #282828;border-top:none;color:#fff;font-size:10pt}#trakt-config .reset{color:#fff}',
     events: {
       save: () => {
-        alert(`${GM_info.script.name} : Settings saved`)
-        location.reload(false)
+        window.alert(`${GM.info.script.name}: settings saved`)
+        GM_config.close()
+        window.location.reload(false)
       }
     }
   })
+  GM.registerMenuCommand('Configure', () => GM_config.open())
 
-  // menu command to open configuration
-  GM_registerMenuCommand(`${GM_info.script.name} - Configure`, () => {
-    GM_config.open()
+  //* Utils
+  const ut = new Utils({
+    name: GM.info.script.name,
+    version: GM.info.script.version,
+    author: 'Felix',
+    color: '#ed1c24',
+    logging: GM_config.get('logging')
   })
+  ut.init('trakt-config')
 
-  // logs
-  const log = message => {
-    if (GM_config.get('logging') === true) {
-      console.log(`${GM_info.script.name}: ${message}`)
-    }
-  }
-
-  // NodeCreationObserver
+  //* NodeCreationObserver
   NodeCreationObserver.init('observed-filtering')
   NodeCreationObserver.onCreation('a[href$="/vip/filtering"]', () => {
     $('.frame-wrapper .sidenav .alert-vip-required').hide()
@@ -77,7 +82,7 @@
     $('a[href$="/vip/filtering"]').removeAttr('href').click(() => {
       $('.frame-wrapper .advanced-filters').toggleClass('open')
       $('.frame-wrapper .frame').toggleClass('with-advanced-filters')
-      log('clicked on Advanced Filters')
+      ut.log('clicked on Advanced Filters')
     })
   })
 })()
