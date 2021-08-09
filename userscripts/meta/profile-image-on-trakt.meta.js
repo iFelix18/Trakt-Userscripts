198f524c4248e400f8719b368ef7d0793571dd48
Davide <19800006+iFelix18@users.noreply.github.com>
2021-08-09 12:40:04 +0200

    1.2.2

--- userscripts/meta/profile-image-on-trakt.meta.js
+++ userscripts/meta/profile-image-on-trakt.meta.js
@@ -3,10 +3,10 @@
 // @name:it         Immagine profilo su Trakt
 // @author          Davide <iFelix18@protonmail.com>
 // @namespace       https://github.com/iFelix18
-// @icon            https://avatars.githubusercontent.com/u/19800006?v=4&s=64
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=trakt.tv
 // @description     Set your favorite movie, or TV series as your profile picture
 // @description:it  Imposta il tuo film preferito, o serie TV come immagine del tuo profilo
 // @copyright       2019, Davide (https://github.com/iFelix18)
 // @license         MIT
-// @version         1.2.1
+// @version         1.2.2
 // ==/UserScript==
