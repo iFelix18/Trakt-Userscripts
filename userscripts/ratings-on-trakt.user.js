384e68ff5c05fcb091099ed07c2df0370a657a39
Davide <19800006+iFelix18@users.noreply.github.com>
2021-08-09 12:40:29 +0200

    2.6.3

--- userscripts/ratings-on-trakt.user.js
+++ userscripts/ratings-on-trakt.user.js
@@ -3,12 +3,12 @@
 // @name:it         Valutazioni su Trakt
 // @author          Davide <iFelix18@protonmail.com>
 // @namespace       https://github.com/iFelix18
-// @icon            https://avatars.githubusercontent.com/u/19800006?v=4&s=64
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=trakt.tv
 // @description     Adds ratings from IMDb, Rotten Tomatoes and Metacritic to Trakt
 // @description:it  Aggiunge valutazioni da IMDb, Rotten Tomatoes e Metacritic a Trakt
 // @copyright       2019, Davide (https://github.com/iFelix18)
 // @license         MIT
-// @version         2.6.2
+// @version         2.6.3
 //
 // @homepageURL     https://github.com/iFelix18/Trakt-Userscripts#readme
 // @supportURL      https://github.com/iFelix18/Trakt-Userscripts/issues
