eaa6f86ba2710f10faf769879bcdf95448fa7a30
Davide <19800006+iFelix18@users.noreply.github.com>
2021-08-09 12:41:09 +0200

    1.0.1

--- userscripts/meta/trakt-search.meta.js
+++ userscripts/meta/trakt-search.meta.js
@@ -3,10 +3,10 @@
 // @name:it         Ricerca Trakt
 // @author          Davide <iFelix18@protonmail.com>
 // @namespace       https://github.com/iFelix18
-// @icon            https://avatars.githubusercontent.com/u/19800006?v=4&s=64
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=trakt.tv
 // @description     Shows the results of a search on Trakt
 // @description:it  Mostra i risultati di una ricerca su Trakt
 // @copyright       2021, Davide (https://github.com/iFelix18)
 // @license         MIT
-// @version         1.0.0
+// @version         1.0.1
 // ==/UserScript==
