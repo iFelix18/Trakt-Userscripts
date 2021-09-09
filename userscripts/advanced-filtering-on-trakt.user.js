7b59ec8006e5e92c4bcdc35b4a553acf6bb7f16b
Davide <19800006+iFelix18@users.noreply.github.com>
2021-09-09 21:46:22 +0200

    1.1.1

--- userscripts/advanced-filtering-on-trakt.user.js
+++ userscripts/advanced-filtering-on-trakt.user.js
@@ -1,44 +1,43 @@
 // ==UserScript==
 // @name            Advanced Filtering on Trakt
 // @name:it         Filtri Avanzati su Trakt
-// @author          Felix
+// @author          Davide <iFelix18@protonmail.com>
 // @namespace       https://github.com/iFelix18
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=trakt.tv
 // @description     Show the Advanced Filtering on Trakt
 // @description:it  Mostra i Filtri Avanzati su Trakt
-// @copyright       2019, Felix (https://github.com/iFelix18)
+// @copyright       2019, Davide (https://github.com/iFelix18)
 // @license         MIT
-// @version         1.1.0
-// @homepageURL     https://git.io/Trakt-Userscripts
-// @homepageURL     https://greasyfork.org/scripts/383595-advanced-filtering-on-trakt
-// @homepageURL     https://openuserjs.org/scripts/iFelix18/Advanced_Filtering_on_Trakt
+// @version         1.1.1
+// @homepage        https://github.com/iFelix18/Trakt-Userscripts#readme
+// @homepageURL     https://github.com/iFelix18/Trakt-Userscripts#readme
 // @supportURL      https://github.com/iFelix18/Trakt-Userscripts/issues
 // @updateURL       https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/meta/advanced-filtering-on-trakt.meta.js
 // @downloadURL     https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/advanced-filtering-on-trakt.user.js
-// @require         https://cdn.jsdelivr.net/gh/greasemonkey/gm4-polyfill@master/gm4-polyfill.min.js
-// @require         https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@master/gm_config.min.js
-// @require         https://cdn.jsdelivr.net/gh/soufianesakhi/node-creation-observer-js@master/release/node-creation-observer-1.2.min.js
-// @require         https://cdn.jsdelivr.net/gh/iFelix18/Userscripts@master/lib/utils/utils.min.js
-// @require         https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js#sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=
+// @require         https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@43fd0fe4de1166f343883511e53546e87840aeaf/gm_config.min.js
+// @require         https://cdn.jsdelivr.net/gh/iFelix18/Userscripts@abce8796cedbe28ac8e072d9824c4b9342985098/lib/utils/utils.min.js
+// @require         https://cdn.jsdelivr.net/npm/gm4-polyfill@1.0.1/gm4-polyfill.min.js#sha256-qmLl2Ly0/+2K+HHP76Ul+Wpy1Z41iKtzptPD1Nt8gSk=
+// @require         https://cdn.jsdelivr.net/npm/node-creation-observer@1.2.0/release/node-creation-observer-latest.js#sha256-OlRWIaZ5LD4UKqMHzIJ8Sc0ctSV2pTIgIvgppQRdNUU=
+// @require         https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js#sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=
 // @match           *://trakt.tv/*
 // @grant           GM.getValue
-// @grant           GM_getValue
-// @grant           GM.setValue
-// @grant           GM_setValue
 // @grant           GM.info
-// @grant           GM_info
 // @grant           GM.registerMenuCommand
+// @grant           GM.setValue
+// @grant           GM_getValue
+// @grant           GM_info
 // @grant           GM_registerMenuCommand
+// @grant           GM_setValue
 // @run-at          document-start
 // @inject-into     page
 // ==/UserScript==
-//
-// Recommended in combination with Darkt, my darker theme for Trakt.
-// More info on: https://git.io/Darkt
 
 /* global GM_config, NodeCreationObserver, MonkeyUtils, $ */
 
 (() => {
   'use strict'
+
+  console.log('Not working script, remove it!')
 
   //* GM_config
   GM_config.init({
@@ -48,12 +47,13 @@
       logging: {
         label: 'Logging',
         section: ['Develop'],
-        labelPos: 'above',
+        labelPos: 'right',
         type: 'checkbox',
         default: false
       }
     },
-    css: '#trakt-config{background-color:#343434;color:#fff}#trakt-config *{font-family:varela round,helvetica neue,Helvetica,Arial,sans-serif}#trakt-config .section_header{background-color:#282828;border:1px solid #282828;border-bottom:none;color:#fff;font-size:10pt}#trakt-config .section_desc{background-color:#282828;border:1px solid #282828;border-top:none;color:#fff;font-size:10pt}#trakt-config .reset{color:#fff}',
+    /* cspell: disable-next-line */
+    css: '#trakt-config{background-color:#343434;color:#fff}#trakt-config *{font-family:varela round,helvetica neue,Helvetica,Arial,sans-serif}#trakt-config .section_header{background-color:#282828;border:1px solid #282828;border-bottom:none;color:#fff;font-size:10pt}#trakt-config .section_desc{background-color:#282828;border:1px solid #282828;border-top:none;color:#fff;font-size:10pt}#trakt-config #trakt-config_field_magic{margin:0 auto;display:block}#trakt-config .reset{color:#fff}',
     events: {
       save: () => {
         window.alert(`${GM.info.script.name}: settings saved`)
@@ -68,7 +68,7 @@
   const MU = new MonkeyUtils({
     name: GM.info.script.name,
     version: GM.info.script.version,
-    author: 'Felix',
+    author: GM.info.script.author,
     color: '#ed1c24',
     logging: GM_config.get('logging')
   })
--- userscripts/meta/advanced-filtering-on-trakt.meta.js
+++ userscripts/meta/advanced-filtering-on-trakt.meta.js
@@ -1,11 +1,9 @@
 // ==UserScript==
-// @name            Advanced Filtering on Trakt
-// @name:it         Filtri Avanzati su Trakt
-// @author          Felix
-// @namespace       https://github.com/iFelix18
-// @description     Show the Advanced Filtering on Trakt
-// @description:it  Mostra i Filtri Avanzati su Trakt
-// @copyright       2019, Felix (https://github.com/iFelix18)
-// @license         MIT
-// @version         1.1.0
+// @name         Advanced Filtering on Trakt
+// @author       Davide <iFelix18@protonmail.com>
+// @namespace    https://github.com/iFelix18
+// @description  Show the Advanced Filtering on Trakt
+// @copyright    2019, Davide (https://github.com/iFelix18)
+// @license      MIT
+// @version      1.1.1
 // ==/UserScript==
