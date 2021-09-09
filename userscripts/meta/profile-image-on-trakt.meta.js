205881ec9e492bf60f5d746cd181556f01c121d8
Davide <19800006+iFelix18@users.noreply.github.com>
2021-09-09 21:55:52 +0200

    1.2.4

--- userscripts/meta/profile-image-on-trakt.meta.js
+++ userscripts/meta/profile-image-on-trakt.meta.js
@@ -1,12 +1,9 @@
 // ==UserScript==
-// @name            Profile image on Trakt
-// @name:it         Immagine profilo su Trakt
-// @author          Davide <iFelix18@protonmail.com>
-// @namespace       https://github.com/iFelix18
-// @icon            https://www.google.com/s2/favicons?sz=64&domain=trakt.tv
-// @description     Set your favorite movie, or TV series as your profile picture
-// @description:it  Imposta il tuo film preferito, o serie TV come immagine del tuo profilo
-// @copyright       2019, Davide (https://github.com/iFelix18)
-// @license         MIT
-// @version         1.2.3
+// @name         Profile image on Trakt
+// @author       Davide <iFelix18@protonmail.com>
+// @namespace    https://github.com/iFelix18
+// @description  Set your favorite movie, or TV series as your profile picture
+// @copyright    2019, Davide (https://github.com/iFelix18)
+// @license      MIT
+// @version      1.2.4
 // ==/UserScript==
--- userscripts/profile-image-on-trakt.user.js
+++ userscripts/profile-image-on-trakt.user.js
@@ -8,32 +8,31 @@
 // @description:it  Imposta il tuo film preferito, o serie TV come immagine del tuo profilo
 // @copyright       2019, Davide (https://github.com/iFelix18)
 // @license         MIT
-// @version         1.2.3
-//
+// @version         1.2.4
+// @homepage        https://github.com/iFelix18/Trakt-Userscripts#readme
 // @homepageURL     https://github.com/iFelix18/Trakt-Userscripts#readme
 // @supportURL      https://github.com/iFelix18/Trakt-Userscripts/issues
 // @updateURL       https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/meta/profile-image-on-trakt.meta.js
 // @downloadURL     https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/profile-image-on-trakt.user.js
-//
-// @require         https://cdn.jsdelivr.net/gh/greasemonkey/gm4-polyfill@a834d46afcc7d6f6297829876423f58bb14a0d97/gm4-polyfill.min.js
 // @require         https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@43fd0fe4de1166f343883511e53546e87840aeaf/gm_config.min.js
 // @require         https://cdn.jsdelivr.net/gh/iFelix18/Userscripts@abce8796cedbe28ac8e072d9824c4b9342985098/lib/utils/utils.min.js
+// @require         https://cdn.jsdelivr.net/npm/gm4-polyfill@1.0.1/gm4-polyfill.min.js#sha256-qmLl2Ly0/+2K+HHP76Ul+Wpy1Z41iKtzptPD1Nt8gSk=
 // @require         https://cdn.jsdelivr.net/npm/node-creation-observer@1.2.0/release/node-creation-observer-latest.js#sha256-OlRWIaZ5LD4UKqMHzIJ8Sc0ctSV2pTIgIvgppQRdNUU=
 // @require         https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js#sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=
-//
 // @match           *://trakt.tv/*
-//
-// @grant           GM.info
-// @grant           GM_info
-// @grant           GM.getValue
-// @grant           GM_getValue
-// @grant           GM.setValue
-// @grant           GM_setValue
+// @compatible      chrome
+// @compatible      edge
+// @compatible      firefox
 // @grant           GM.deleteValue
-// @grant           GM_deleteValue
+// @grant           GM.getValue
+// @grant           GM.info
 // @grant           GM.registerMenuCommand
+// @grant           GM.setValue
+// @grant           GM_deleteValue
+// @grant           GM_getValue
+// @grant           GM_info
 // @grant           GM_registerMenuCommand
-//
+// @grant           GM_setValue
 // @run-at          document-idle
 // @inject-into     page
 // ==/UserScript==
@@ -50,12 +49,13 @@
     fields: {
       logging: {
         label: 'Logging',
-        labelPos: 'above',
+        labelPos: 'right',
         type: 'checkbox',
         default: false
       }
     },
-    css: '#trakt-config{background-color:#343434;color:#fff}#trakt-config *{font-family:varela round,helvetica neue,Helvetica,Arial,sans-serif}#trakt-config .section_header{background-color:#282828;border:1px solid #282828;border-bottom:none;color:#fff;font-size:10pt}#trakt-config .section_desc{background-color:#282828;border:1px solid #282828;border-top:none;color:#fff;font-size:10pt}#trakt-config .reset{color:#fff}',
+    /* cSpell: disable-next-line */
+    css: '#trakt-config{background-color:#343434;color:#fff}#trakt-config *{font-family:varela round,helvetica neue,Helvetica,Arial,sans-serif}#trakt-config .section_header{background-color:#282828;border:1px solid #282828;border-bottom:none;color:#fff;font-size:10pt}#trakt-config .section_desc{background-color:#282828;border:1px solid #282828;border-top:none;color:#fff;font-size:10pt}#trakt-config #trakt-config_field_magic{margin:0 auto;display:block}#trakt-config .reset{color:#fff}',
     events: {
       save: () => {
         window.alert(`${GM.info.script.name}: settings saved`)
