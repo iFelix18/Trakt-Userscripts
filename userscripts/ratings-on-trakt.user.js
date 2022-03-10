846e3b580ffb0e5d4b1e92abf9ee840094356358
Davide <19800006+iFelix18@users.noreply.github.com>
2022-03-10 23:50:47 +0100

    4.6.0

--- userscripts/ratings-on-trakt.user.js
+++ userscripts/ratings-on-trakt.user.js
@@ -1,133 +1,155 @@
 // ==UserScript==
-// @name            Ratings on Trakt
-// @name:it         Valutazioni su Trakt
-// @author          Davide <iFelix18@protonmail.com>
-// @namespace       https://github.com/iFelix18
-// @icon            https://www.google.com/s2/favicons?sz=64&domain=https://trakt.tv
-// @description     Adds ratings from IMDb, Rotten Tomatoes, Metacritic and MyAnimeList to Trakt
-// @description:it  Aggiunge valutazioni da IMDb, Rotten Tomatoes, Metacritic e MyAnimeList a Trakt
-// @copyright       2019, Davide (https://github.com/iFelix18)
-// @license         MIT
-// @version         4.5.0
-// @homepage        https://github.com/iFelix18/Trakt-Userscripts#readme
-// @homepageURL     https://github.com/iFelix18/Trakt-Userscripts#readme
-// @supportURL      https://github.com/iFelix18/Trakt-Userscripts/issues
-// @updateURL       https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/meta/ratings-on-trakt.meta.js
-// @downloadURL     https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/ratings-on-trakt.user.js
-// @require         https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@43fd0fe4de1166f343883511e53546e87840aeaf/gm_config.min.js
-// @require         https://cdn.jsdelivr.net/gh/iFelix18/Userscripts@utils-3.0.2/lib/utils/utils.min.js
-// @require         https://cdn.jsdelivr.net/gh/iFelix18/Userscripts@omdb-1.2.6/lib/api/omdb.min.js
-// @require         https://cdn.jsdelivr.net/gh/iFelix18/Userscripts@rottentomatoes-1.1.6/lib/api/rottentomatoes.min.js
-// @require         https://cdn.jsdelivr.net/gh/iFelix18/Userscripts@jikan-1.0.2/lib/api/jikan.min.js
-// @require         https://cdn.jsdelivr.net/gh/iFelix18/Userscripts@ratings-3.0.0/lib/utils/ratings.min.js
-// @require         https://cdn.jsdelivr.net/npm/node-creation-observer@1.2.0/release/node-creation-observer-latest.min.js
-// @require         https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js
-// @require         https://cdn.jsdelivr.net/npm/handlebars@4.7.7/dist/handlebars.min.js
-// @match           *://trakt.tv/*
-// @connect         api.jikan.moe
-// @connect         omdbapi.com
-// @connect         rottentomatoes.com
-// @grant           GM_getValue
-// @grant           GM_setValue
-// @grant           GM.deleteValue
-// @grant           GM.getValue
-// @grant           GM.listValues
-// @grant           GM.registerMenuCommand
-// @grant           GM.setValue
-// @grant           GM.xmlHttpRequest
-// @run-at          document-start
-// @inject-into     content
+// @name               Ratings on Trakt
+// @name:de            Bewertungen auf Trakt
+// @name:es            Calificaciones en Trakt
+// @name:fr            Évaluations sur Trakt
+// @name:it            Valutazioni su Trakt
+// @name:ru            Рейтинги на Trakt
+// @name:zh-CN         Trakt上的评分
+// @author             Davide <iFelix18@protonmail.com>
+// @namespace          https://github.com/iFelix18
+// @icon               https://www.google.com/s2/favicons?sz=64&domain=https://trakt.tv
+// @description        Adds ratings from IMDb, Rotten Tomatoes, Metacritic and MyAnimeList to Trakt
+// @description:de     Fügt Bewertungen von IMDb, Rotten Tomatoes, Metacritic und MyAnimeList zu Trakt hinzu
+// @description:es     Agrega las calificaciones de IMDb, Rotten Tomatoes, Metacritic y MyAnimeList a Trakt
+// @description:fr     Ajoute des évaluations d'IMDb, Rotten Tomatoes, Metacritic et MyAnimeList à Trakt
+// @description:it     Aggiunge le valutazioni di IMDb, Rotten Tomatoes, Metacritic e MyAnimeList a Trakt
+// @description:ru     Добавляет рейтинги IMDb, Rotten Tomatoes, Metacritic и MyAnimeList в Trakt
+// @description:zh-CN  在Trakt中添加来自IMDb、烂番茄、Metacritic和MyAnimeList的评分。
+// @copyright          2019, Davide (https://github.com/iFelix18)
+// @license            MIT
+// @version            4.6.0
+// @homepage           https://github.com/iFelix18/Trakt-Userscripts#readme
+// @homepageURL        https://github.com/iFelix18/Trakt-Userscripts#readme
+// @supportURL         https://github.com/iFelix18/Trakt-Userscripts/issues
+// @updateURL          https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/meta/ratings-on-trakt.meta.js
+// @downloadURL        https://raw.githubusercontent.com/iFelix18/Trakt-Userscripts/master/userscripts/ratings-on-trakt.user.js
+// @require            https://cdn.jsdelivr.net/gh/sizzlemctwizzle/GM_config@43fd0fe4de1166f343883511e53546e87840aeaf/gm_config.min.js
+// @require            https://cdn.jsdelivr.net/npm/@ifelix18/utils@5.1.1/lib/index.min.js
+// @require            https://cdn.jsdelivr.net/npm/@ifelix18/omdb@2.0.0/lib/index.min.js
+// @require            https://cdn.jsdelivr.net/npm/@ifelix18/rottentomatoes@2.0.0/lib/index.min.js
+// @require            https://cdn.jsdelivr.net/npm/@ifelix18/jikan@2.0.0/lib/index.min.js
+// @require            https://cdn.jsdelivr.net/npm/@ifelix18/ratings@4.0.0/lib/index.min.js
+// @require            https://cdn.jsdelivr.net/npm/node-creation-observer@1.2.0/release/node-creation-observer-latest.min.js
+// @require            https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js
+// @require            https://cdn.jsdelivr.net/npm/handlebars@4.7.7/dist/handlebars.min.js
+// @match              *://trakt.tv/*
+// @connect            api.jikan.moe
+// @connect            omdbapi.com
+// @connect            rottentomatoes.com
+// @compatible         chrome
+// @compatible         edge
+// @compatible         firefox
+// @compatible         safari
+// @grant              GM_getValue
+// @grant              GM_setValue
+// @grant              GM.deleteValue
+// @grant              GM.getValue
+// @grant              GM.listValues
+// @grant              GM.registerMenuCommand
+// @grant              GM.setValue
+// @grant              GM.xmlHttpRequest
+// @run-at             document-start
+// @inject-into        content
 // ==/UserScript==
 
-/* global $, GM_config, Handlebars, migrateConfig, MyUtils, NodeCreationObserver, Ratings */
+/* global $, GM_configStruct, Handlebars, NodeCreationObserver, Ratings, UserscriptUtils */
 
 (() => {
-  migrateConfig('trakt-config', 'ratings-on-trakt') // migrate to the new config ID
+  //* Constants
+  const cachePeriod = 3_600_000
+  const id = GM.info.script.name.toLowerCase().replace(/\s/g, '-')
 
   //* GM_config
-  GM_config.init({
-    id: 'ratings-on-trakt',
-    title: `${GM.info.script.name} v${GM.info.script.version} Settings`,
-    fields: {
-      OMDbApiKey: {
-        label: 'OMDb API Key',
-        section: ['You can request a free OMDb API Key at:', 'https://www.omdbapi.com/apikey.aspx'],
-        labelPos: 'left',
-        type: 'text',
-        title: 'Your OMDb API Key',
-        size: 70,
-        default: ''
-      },
-      hideDefaultRatings: {
-        label: 'Prefer the ratings offered by this script to those offered by Trakt',
-        section: ['Features'],
-        labelPos: 'right',
-        type: 'checkbox',
-        default: true
-      },
-      logging: {
-        label: 'Logging',
-        section: ['Develop'],
-        labelPos: 'right',
-        type: 'checkbox',
-        default: false
-      },
-      debugging: {
-        label: 'Debugging',
-        labelPos: 'right',
-        type: 'checkbox',
-        default: false
-      },
-      clearCache: {
-        label: 'Clear the cache',
-        type: 'button',
-        click: async () => {
-          const values = await GM.listValues()
-
-          for (const value of values) {
-            const cache = await GM.getValue(value) // get cache
-            if (cache.time) { GM.deleteValue(value) } // delete cache
-          }
-
-          MU.log('cache cleared')
-          GM_config.close()
-        }
-      }
+  const config = new GM_configStruct()
+  const fields = {
+    OMDbApiKey: {
+      label: 'API Key:',
+      section: ['OMDb', 'You can request a free OMDb API Key at: https://www.omdbapi.com/apikey.aspx'],
+      labelPos: 'left',
+      type: 'text',
+      title: 'Your OMDb API Key',
+      size: 70,
+      default: ''
     },
-    css: ':root{--mainBackground:#343433;--background:#282828;--text:#fff}body{background-color:var(--mainBackground)!important;color:var(--text)!important}body .section_header{background-color:var(--background)!important;border-bottom:none!important;border:1px solid var(--background)!important;color:var(--text)!important}body .section_desc{background-color:var(--background)!important;border-top:none!important;border:1px solid var(--background)!important;color:var(--text)!important}body .reset{color:var(--text)!important}',
+    hideDefaultRatings: {
+      label: 'Prefer the ratings offered by this script to those offered by Trakt:',
+      section: ['Features'],
+      labelPos: 'left',
+      type: 'checkbox',
+      default: true
+    },
+    logging: {
+      label: 'Logging:',
+      section: ['Develop'],
+      labelPos: 'left',
+      type: 'checkbox',
+      default: false
+    },
+    debugging: {
+      label: 'Debugging:',
+      labelPos: 'left',
+      type: 'checkbox',
+      default: false
+    },
+    clearCache: {
+      label: 'Clear the cache',
+      type: 'button',
+      click: async () => {
+        const values = await GM.listValues()
+
+        for (const value of values) {
+          const cache = await GM.getValue(value) // get cache
+          if (cache.time) { GM.deleteValue(value) } // delete cache
+        }
+
+        UU.log('cache cleared')
+      }
+    }
+  }
+  const title = `${GM.info.script.name} v${GM.info.script.version} Settings`
+
+  config.init({
+    id,
+    title,
+    fields,
     events: {
       init: () => {
-        if (!GM_config.isOpen && GM_config.get('OMDbApiKey') === '') {
-          window.addEventListener('load', () => GM_config.open())
+        if (!config.isOpen && config.get('OMDbApiKey') === '') {
+          window.location = `/settings/${id}/`
+        }
+        if (GM.info.scriptHandler !== 'Userscripts') { //! Userscripts Safari: GM.registerMenuCommand is missing
+          GM.registerMenuCommand('Configure', () => config.open())
         }
       },
       save: () => {
-        if (!GM_config.isOpen && GM_config.get('OMDbApiKey') === '') {
+        if (config.get('OMDbApiKey') === '') {
           window.alert(`${GM.info.script.name}: check your settings and save`)
         } else {
           window.alert(`${GM.info.script.name}: settings saved`)
-          GM_config.close()
-          window.location.reload(false)
+          config.close()
+          setTimeout(window.location.reload(false), 500)
         }
       }
     }
   })
-  if (GM.info.scriptHandler !== 'Userscripts') GM.registerMenuCommand('Configure', () => GM_config.open()) //! Userscripts Safari: GM.registerMenuCommand is missing
 
-  //* MyUtils
-  const MU = new MyUtils({
+  UserscriptUtils.migrateConfig('trakt-config', id) // migrate to the new config ID
+
+  //* Utils
+  const UU = new UserscriptUtils({
     name: GM.info.script.name,
     version: GM.info.script.version,
     author: GM.info.script.author,
-    color: '#ed1c24',
-    logging: GM_config.get('logging')
+    logging: config.get('logging')
   })
-  MU.init('ratings-on-trakt')
+  UU.init(id)
 
   //* Ratings
   const rating = new Ratings({
-    omdb_apikey: GM_config.get('OMDbApiKey'),
-    debug: GM_config.get('debugging')
+    omdb_api_key: config.get('OMDbApiKey'),
+    cache_period: cachePeriod,
+    debug: config.get('debugging')
   })
 
   //* Handlebars
@@ -138,12 +160,55 @@
 
   //* Functions
   /**
-   * Adds a button for script configuration to the menu
+   * Add style
    */
-  const addMenu = () => {
-    const menu = `<li class='${GM.info.script.name.toLowerCase().replace(/\s/g, '_')}'><a href='' onclick='return false;'>${GM.info.script.name}</a></li>`
+  const addStyle = () => {
+    const style = '<style>@import url(https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap);header#top-nav .navbar-nav.navbar-user:hover #user-menu{max-height:max-content}</style>'
+
+    $('head').append(style)
+  }
+
+  /**
+   * Adds a link to the menu to access the script configuration
+   */
+  const addSettingsToMenu = () => {
+    const menu = `<li class=${id}><a href=/settings/${id}/>${GM.info.script.name}</a>`
+
     $('#user-menu ul li.separator').last().after(menu)
-    $(`.${GM.info.script.name.toLowerCase().replace(/\s/g, '_')}`).click(() => GM_config.open())
+  }
+
+  /**
+   * Adds settings
+   */
+  const addSettings = () => {
+    document.title = title // settings page title
+
+    $('html').css('background-color', 'rgb(29, 29, 29)') // background
+
+    config.init({ // GM_config
+      frame: $('body').empty().get(0),
+      id,
+      title,
+      fields,
+      css: ':root{--font:"Montserrat",sans-serif!important;--black:rgb(0, 0, 0)!important;--dark-grey:rgb(22, 22, 22)!important;--grey:rgb(51, 51, 51)!important;--light-grey:rgb(102, 102, 102)!important;--red:rgb(237, 34, 36)!important;--white:rgb(255, 255, 255)!important}#ratings-on-trakt *{color:var(--white)!important;font-family:var(--font)!important;font-size:14px!important}#ratings-on-trakt{background-color:transparent!important;border:1px solid transparent!important;box-shadow:0 1px 1px rgba(0,0,0,.05)!important;box-sizing:border-box!important;height:auto!important;list-style-type:none!important;margin-bottom:0!important;margin-left:auto!important;margin-right:auto!important;margin-top:1em!important;max-height:none!important;max-width:1200px!important;padding:1em!important;position:static!important;width:auto!important}#ratings-on-trakt .config_header{color:var(--white)!important;font-size:34px!important;font-weight:400!important;line-height:1.1!important;margin:0!important;text-shadow:0 0 20px var(--black)!important}#ratings-on-trakt .section_header_holder{background-color:var(--dark-grey)!important;border:1px solid var(--grey)!important;margin-bottom:1em!important}#ratings-on-trakt .section_header{background-color:var(--grey)!important;background-image:none!important;border-bottom:1px solid transparent!important;border:1px solid var(--grey)!important;padding:8px!important;text-align:left!important;text-transform:uppercase!important}#ratings-on-trakt .section_desc{background-color:var(--black)!important;border:1px solid var(--grey)!important;border-left:0!important;border-right:0!important;font-size:13px!important;font-weight:400!important;margin:0!important;padding:10px 8px!important;text-align:left!important}#ratings-on-trakt .config_var{align-items:center!important;display:flex!important;margin:0!important;padding:15px!important}#ratings-on-trakt .field_label{margin-bottom:0!important;margin-left:6px!important}#ratings-on-trakt_field_OMDbApiKey{background-color:var(--grey)!important;border:1px solid var(--light-grey)!important;box-shadow:inset 0 1px 1px rgba(0,0,0,.075)!important;flex:1!important;padding:6px 12px!important}#ratings-on-trakt_field_OMDbApiKey:focus{box-shadow:inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6)!important;outline:0!important}#ratings-on-trakt_buttons_holder{padding:15px!important}#ratings-on-trakt button,#ratings-on-trakt input[type=button]{background-color:var(--grey)!important;background-image:none!important;border-radius:0!important;border:1px solid transparent!important;font-weight:400!important;text-align:center!important;vertical-align:middle!important;padding:10px 16px!important}#ratings-on-trakt_buttons_holder button{background-color:var(--red)!important}#ratings-on-trakt_closeBtn{display:none!important}#ratings-on-trakt .reset{margin-right:10px!important}',
+      events: {
+        init: () => {
+          config.open()
+        },
+        save: () => {
+          if (config.get('OMDbApiKey') === '') {
+            window.alert(`${GM.info.script.name}: check your settings and save`)
+          } else {
+            window.alert(`${GM.info.script.name}: settings saved`)
+            if ('referrer' in document && document.referrer !== '') {
+              window.location = document.referrer
+            } else {
+              window.history.back()
+            }
+          }
+        }
+      }
+    })
   }
 
   /**
@@ -168,11 +233,10 @@
   /**
    * Returns IMDb ID
    *
-   * @param {string} link IMDb link
    * @returns {string} IMDb ID
    */
-  const getID = (link) => {
-    return link.match(/tt\d+/)[0]
+  const getID = () => {
+    return $('#info-wrapper .sidebar .external li a#external-link-imdb').attr('href').match(/tt\d+/)[0]
   }
 
   /**
@@ -187,56 +251,45 @@
   }
 
   /**
-   * Compile template
-   *
-   * @param {object} ratings Ratings
+   * Add ratings
    */
-  const compileTemplate = (ratings) => {
-    const template = Handlebars.compile($('#external-ratings-template').html()) // compile template
-    const context = { ratings: ratings }
-    const compile = template(context)
-    $('.external-ratings').html(compile)
-    if (GM_config.get('hideDefaultRatings')) hideDefaultRatings() // hide default ratings by Trakt
-  }
-
-  /**
-   * Main function
-   */
-  const main = async () => {
-    clearOldCache()
-
-    const link = $('#info-wrapper .sidebar .external li a#external-link-imdb').attr('href') // IMDb link
-    if (!link) return // check if the link exists
-    const id = getID(link) // IMDb ID
-    if (!id) return // check if an ID exists
-    MU.log(`ID is '${id}'`)
+  const addRatings = async () => {
+    clearOldCache() // clear old cache
 
     const target = $('#summary-ratings-wrapper .ratings') // target
-    if (target.length === 0) return // check if the target exists
+    if (target.length === 0) return // check if it is on the main page
+
+    const id = getID() // IMDb ID
+    if (!id) return // check if the ID exists
+    UU.log(`ID is '${id}'`)
+
     addTemplate(target) // add template
 
-    const cache = await GM.getValue(id) // get cache
+    // get ratings
+    const ratings = await rating.get({ id }).then().catch(error => console.error(error))
+    const elaboratedRatings = await rating.elaborate(ratings).then().catch(error => console.error(error))
 
-    if (cache !== undefined && ((Date.now() - cache.time) < 3_600_000) && !GM_config.get('debugging')) { // cache valid
-      MU.log(`${id} data from cache`)
+    // compile template
+    const template = Handlebars.compile($('#external-ratings-template').html())
+    const context = { ratings: elaboratedRatings }
+    const compile = template(context)
+    $('.external-ratings').html(compile)
 
-      compileTemplate(cache.ratings)
-    } else { // cache not valid
-      MU.log(`${id} data from API`)
-
-      const data = await rating.get(id).then() // get data ratings
-      const ratings = await rating.elaborate(data).then() // elaborate ratings
-
-      if (!GM_config.get('debugging')) GM.setValue(id, { ratings, time: Date.now() }) // set cache
-
-      compileTemplate(ratings)
-    }
+    if (config.get('hideDefaultRatings')) hideDefaultRatings() // hide default ratings by Trakt
   }
 
   //* Script
   $(document).ready(() => {
     NodeCreationObserver.init(GM.info.script.name.toLowerCase().replace(/\s/g, '_'))
-    NodeCreationObserver.onCreation('#user-menu ul', () => addMenu())
-    NodeCreationObserver.onCreation('.movies.show #summary-ratings-wrapper, .shows.show #summary-ratings-wrapper, .shows.episode #summary-ratings-wrapper', () => main()) // add ratings
+    NodeCreationObserver.onCreation('body', () => {
+      addStyle() // add style for settings
+      addSettingsToMenu()
+      if (document.location.pathname === `/settings/${id}/`) {
+        addSettings() // add settings
+      }
+    })
+    NodeCreationObserver.onCreation('.movies.show #summary-ratings-wrapper, .shows.show #summary-ratings-wrapper, .shows.episode #summary-ratings-wrapper', () => {
+      addRatings() // add ratings
+    })
   })
 })()
